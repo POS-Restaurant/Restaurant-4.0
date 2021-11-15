d598ced3c448e127b47dea7e3edac7471657e842
duonglam306 <lamduong11201@gmail.com>
2021-11-15 17:45:06 +0700

    up

--- React/myapp/src/components/Sidebar.js
+++ React/myapp/src/components/Sidebar.js
@@ -51,11 +51,29 @@ const SidebarWrap = styled.div`
 
 function Sidebar({type}) {
   const [sidebar, setSidebar] = useState(false);
-
   const showSidebar = () =>{setSidebar(!sidebar);
     let x = document.querySelector(".MenuCard")
     if (x !=null)
         x.classList.toggle('active');
+  }
+  localStorage.setItem('role',0);
+  function show(){
+    if (localStorage.getItem('role') == 0)
+      return SidebarDataCus.map((item, index) => {
+        return <SubMenu item={item} key={index} />;
+      })
+    if (localStorage.getItem('role') == 1)
+      return SidebarDataChef.map((item, index) => {
+        return <SubMenu item={item} key={index} />;
+      })
+      if (localStorage.getItem('role') == 2)
+      return SidebarDataMngr.map((item, index) => {
+        return <SubMenu item={item} key={index} />;
+      })
+      if (localStorage.getItem('role') == 3)
+      return SidebarDataAdmin.map((item, index) => {
+        return <SubMenu item={item} key={index} />;
+      })
   }
   return (
     <>
@@ -71,30 +89,10 @@ function Sidebar({type}) {
             <NavIcon to='#'>
               <AiIcons.AiOutlineClose onClick={showSidebar} />
             </NavIcon>
-            {/* {SidebarDataCus.map((item, index) => {
+            {/* {type1.map((item, index) => {
               return <SubMenu item={item} key={index} />;
             })} */}
-            {
-              function show(){
-                var role = 0;
-                if (localStorage.getItem('role') == 0)
-                  return SidebarDataCus.map((item, index) => {
-                    return <SubMenu item={item} key={index} />;
-                  })
-                if (localStorage.getItem('role') == 1)
-                  return SidebarDataChef.map((item, index) => {
-                    return <SubMenu item={item} key={index} />;
-                  })
-                  if (localStorage.getItem('role') == 2)
-                  return SidebarDataMngr.map((item, index) => {
-                    return <SubMenu item={item} key={index} />;
-                  })
-                  if (localStorage.getItem('role') == 3)
-                  return SidebarDataAdmin.map((item, index) => {
-                    return <SubMenu item={item} key={index} />;
-                  })
-              }
-            }
+            {show()}
           </SidebarWrap>
         </SidebarNav>
       </IconContext.Provider>
