f9db791a41424afc7e6babc51a8d0986aad3b116
trungkien2602 <trungkien260201@gmail.com>
2021-11-16 17:44:20 +0700

    editEditMenu

--- React/myapp/src/pages/EditMenu/FoodItem.js
+++ React/myapp/src/pages/EditMenu/FoodItem.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import styles from './EditMenu.module.css'
 
-const FoodItem = ({name,src,price,protein,material,decoration,onClick}) => {
+const FoodItem = (props) => {
     return (
         <div className= {styles.itemContainer}>
             <div className={styles.itemBody}>
-                <img className={styles.itemPic} src={src} alt="FoodPicture" />               
+                <img className={styles.itemPic} src={props.src} alt="FoodPicture" />               
                 <div className={styles.itemInfo}>
-                    <div className={styles.itemName}>{name}</div>
-                    <div className={styles.itemCharInfo} > <span className={styles.itemLabelInfo}>Protein: </span> {protein}</div>
-                    <div className={styles.itemCharInfo} > <span className={styles.itemLabelInfo}>Material: </span> {material}</div>
-                    <div className={styles.itemCharInfo} > <span className={styles.itemLabelInfo}>Decoration: </span> {decoration}</div>
+                    <div className={styles.itemName}>{props.name}</div>
+                    <div className={styles.itemCharInfo} > <span className={styles.itemLabelInfo}>Protein: </span> {props.rotein}</div>
+                    <div className={styles.itemCharInfo} > <span className={styles.itemLabelInfo}>Material: </span> {props.material}</div>
+                    <div className={styles.itemCharInfo} > <span className={styles.itemLabelInfo}>Decoration: </span> {props.decoration}</div>
                     
-                    <div className={styles.itemPrice} >{price}đ</div>
+                    <div className={styles.itemPrice} >{props.price}đ</div>
                 </div>
 
                 
             </div>
-            <button onClick={onClick} className={`${styles.btn} ${styles.btn1}`} >Chỉnh sửa</button>
+            <button onClick={props.onClick} className={`${styles.btn} ${styles.btn1}`} >Chỉnh sửa</button>
 
         </div>
     )
