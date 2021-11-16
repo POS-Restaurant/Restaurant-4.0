5ea3e7b4d03b58ea7130fc9659e7be651fe4e52d
duonglam306 <lamduong11201@gmail.com>
2021-11-16 19:48:37 +0700

    up

--- React/myapp/src/pages/Chef/ChefUI.js
+++ React/myapp/src/pages/Chef/ChefUI.js
@@ -7,7 +7,7 @@ import Pagination from "react-js-pagination";
 //import ConfirmState from "./ConfirmState";
 import OrderDetail from "./OrderDetail/OrderDetail";
 // import Stack from '@mui/material/Stack';
-//
+import Sidebar from "../../components/Sidebar"
 const DUMMY_DATA = [
     {
         id: 123456756,
@@ -90,77 +90,80 @@ const DUMMY_DATA = [
 
 function ChefUI() {
     return (
-        <div class={chef.chef_ui}>
-            <header className={chef.header}>
-                <h1 className={chef.title}>Quản lý đơn hàng</h1>
-                <div className={chef.right}>
-                    <div className={chef.searchBar}>
-                        <input className={chef.inputSearch} type="text" />{" "}
-                        <button className={chef.searchButton}>
-                            <span
-                                className={`${chef.materialIconsOutlined} ${chef.buttonSearch}`}
-                            >
-                                search
+        <div>
+            <Sidebar type={1}/>
+            <div class={chef.chef_ui}>
+                <header className={chef.header}>
+                    <h1 className={chef.title}>Quản lý đơn hàng</h1>
+                    <div className={chef.right}>
+                        <div className={chef.searchBar}>
+                            <input className={chef.inputSearch} type="text" />{" "}
+                            <button className={chef.searchButton}>
+                                <span
+                                    className={`${chef.materialIconsOutlined} ${chef.buttonSearch}`}
+                                >
+                                    search
+                                </span>
+                            </button>
+                        </div>
+                        <button className={chef.notiButton}>
+                            <span className={chef.materialIconsOutlined}>
+                                notifications
                             </span>
                         </button>
                     </div>
-                    <button className={chef.notiButton}>
-                        <span className={chef.materialIconsOutlined}>
-                            notifications
-                        </span>
-                    </button>
-                </div>
-            </header>
-            <div className={chef.mainContent}>
-                <div className={chef.buttonBar}>
-                    <button className={`${chef.btnAll} ${chef.active}`}>
-                        Tất cả
-                    </button>
-                    <button className={chef.btnPending}>Đang chờ</button>
-                    <button className={chef.btnDoing}>Đang thực hiện</button>
-                    <button className={chef.btnDone}>Hoàn thành</button>
-                    <button className={chef.btnCancel}>Đã hủy</button>
-                </div>
-                <div className={chef.orderTable}>
-                    <div
-                        className={`${chef.orderListInfoBar} ${chef.orderCard}`}
-                    >
-                        <div className={chef.orderCardID}>Mã đơn</div>
-                        <div className={chef.orderCardTime}>Thời gian đặt</div>
-                        <div className={chef.orderCardName}>Người đặt</div>
-                        <div className={chef.orderCardPrice}>Tổng tiền</div>
-                        <div className={chef.orderCardStatus}>Trạng thái</div>
-                        <div className={chef.orderUpdateState}></div>
+                </header>
+                <div className={chef.mainContent}>
+                    <div className={chef.buttonBar}>
+                        <button className={`${chef.btnAll} ${chef.active}`}>
+                            Tất cả
+                        </button>
+                        <button className={chef.btnPending}>Đang chờ</button>
+                        <button className={chef.btnDoing}>Đang thực hiện</button>
+                        <button className={chef.btnDone}>Hoàn thành</button>
+                        <button className={chef.btnCancel}>Đã hủy</button>
                     </div>
-                    <div className={chef.orderList}>
-                        {DUMMY_DATA.map((order) => {
-                            return (
-                                <OrderCard
-                                    key={order.id}
-                                    id={order.id}
-                                    time={order.time}
-                                    name={order.name}
-                                    price={order.price}
-                                    status={order.status}
-                                />
-                            );
-                        })}
+                    <div className={chef.orderTable}>
+                        <div
+                            className={`${chef.orderListInfoBar} ${chef.orderCard}`}
+                        >
+                            <div className={chef.orderCardID}>Mã đơn</div>
+                            <div className={chef.orderCardTime}>Thời gian đặt</div>
+                            <div className={chef.orderCardName}>Người đặt</div>
+                            <div className={chef.orderCardPrice}>Tổng tiền</div>
+                            <div className={chef.orderCardStatus}>Trạng thái</div>
+                            <div className={chef.orderUpdateState}></div>
+                        </div>
+                        <div className={chef.orderList}>
+                            {DUMMY_DATA.map((order) => {
+                                return (
+                                    <OrderCard
+                                        key={order.id}
+                                        id={order.id}
+                                        time={order.time}
+                                        name={order.name}
+                                        price={order.price}
+                                        status={order.status}
+                                    />
+                                );
+                            })}
+                        </div>
                     </div>
+                    {/* <div className={chef.paging}>
+                        <Pagination itemsCountPerPage={10}
+                        totalItemsCount={450}
+                        pageRangeDisplayed={5} />
+                    </div> */}
+                    {/* <Pagination
+                        activePage={this.state.activePage}
+                        itemsCountPerPage={10}
+                        totalItemsCount={450}
+                        pageRangeDisplayed={5}
+                        onChange={this.handlePageChange.bind(this)}
+                    /> */}
                 </div>
-                {/* <div className={chef.paging}>
-                    <Pagination itemsCountPerPage={10}
-                    totalItemsCount={450}
-                    pageRangeDisplayed={5} />
-                </div> */}
-                {/* <Pagination
-                    activePage={this.state.activePage}
-                    itemsCountPerPage={10}
-                    totalItemsCount={450}
-                    pageRangeDisplayed={5}
-                    onChange={this.handlePageChange.bind(this)}
-                /> */}
+                <OrderDetail/>
             </div>
-            <OrderDetail/>
         </div>
     );
 }
