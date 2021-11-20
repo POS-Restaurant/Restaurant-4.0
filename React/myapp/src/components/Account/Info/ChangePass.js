7d3b3a2f62b4525d32fe5f2d1c2a59225199e4c6
dang-416 <59189549+dang-416@users.noreply.github.com>
2021-11-21 00:12:42 +0700

    userModel

--- React/myapp/src/components/Account/Info/ChangePass.js
+++ React/myapp/src/components/Account/Info/ChangePass.js
@@ -1,32 +1,98 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Input } from "reactstrap";
 import style from "../Account.module.css";
 import Button from "@mui/material/Button";
+import { useRef } from "react";
+import axios from 'axios';
 
 function ChangePass(props) {
+    // const oldPass = useRef();
+    // const newPass = useRef();
+    // const newConfirmPass = useRef();
+
+    const [newPass, setNewPass] = useState("");
+    const [oldPass, setoldPass] = useState("");
+    const [newPassConfirm, setNewPassConfirm] = useState("");
+    const [oldPassErr, setOldPassErr] = useState("");
+    const [newPassErr, setNewPassErr] = useState("");
+
+    function resetPassword(oldP, newP, newConfirm) {
+        //kiem tra password cu, sai quang exception
+        //kiem tra password moi va password confirm, sai quang exception
+        //send update()
+    }
+    function onSubmit(event) {
+        event.preventDefault();
+
+        setOldPassErr("");
+        setNewPassErr("");
+
+        const oldP = oldPass;
+        const newP = newPass;
+        const newConfirm = newPassConfirm;
+
+        const changePassReq = {
+            oldPass: oldP,
+            newPass: newP,
+            newPassConfirm: newConfirm,
+        };
+
+        //send request to reset password
+        axios.post("/change_pass", {id: localStorage.getItem("id"), newPassword: newP})
+
+        console.log(changePassReq);
+    }
+
     return (
         <div className={style.changePass}>
             <h1>Sửa mật khẩu</h1>
-            <form className={style.changePassForm}>
-                <div className={style.passField}>
-                    <label>Nhập mật khẩu cũ</label>
-                    <Input name="Name" type="password" required />
+            <form className={style.changePassForm} onSubmit={onSubmit}>
+                <div className={style.control}>
+                    <label htmlFor="oldPass">Nhập mật khẩu cũ</label>
+                    <Input
+                        id="oldPass"
+                        type="password"
+                        innerRef={(input) => setoldPass(input)}
+                        required
+                        // ref={oldPass}
+                    />
+                    {oldPassErr && <span>oldPassErr</span>}
                 </div>
-                <div className={style.passField}>
-                    <label>Nhập mật khẩu mới</label>
-                    <Input name="Name" type="password" required />
+                <div className={style.control}>
+                    <label htmlFor="newPass">Nhập mật khẩu mới</label>
+                    <Input
+                        id="newPass"
+                        type="password"
+                        innerRef={(input) => setNewPass(input)}
+                        required
+                        // ref={newPass}
+                    />
                 </div>
-                <div className={style.passField}>
-                    <label>Xác nhận mật khẩu mới</label>
-                    <Input name="Phone" type="password" required />
+                <div className={style.control}>
+                    <label htmlFor="newConfirmPass">
+                        Xác nhận mật khẩu mới
+                    </label>
+                    <Input
+                        id="newConfirmPass"
+                        type="password"
+                        innerRef={(input) => setNewPassConfirm(input)}
+                        required
+                        // ref={newConfirmPass}
+                    />
+                    {newPassErr && <span>newPassErr</span>}
                 </div>
-                <Button
-                    className={style.changePassConfirm}
-                    variant="contained"
-                    size="large"
-                >
-                    Sửa mật khẩu
-                </Button>
+                <input type="submit" />
+                <div className={style.actions}>
+                    {" "}
+                    <Button
+                        onClick={onSubmit}
+                        className={style.changePassConfirm}
+                        variant="contained"
+                        size="large"
+                    >
+                        Sửa mật khẩu
+                    </Button>
+                </div>
             </form>
 
             <Button
--- React/myapp/src/components/Sign/Login/Login.js
+++ React/myapp/src/components/Sign/Login/Login.js
@@ -1,11 +1,14 @@
 // import React, { Component } from "react";
-// import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 // import styled from "styled-components";
 import { Input, Row, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 import sign from "../Sign.module.css";
-import { typeUser } from './prelogin';
+import { typeUser } from "./prelogin";
+import { useState } from "react";
+import axios from "axios";
 function Login(props) {
+    const [id, setId] = useState("");
     function handleUserType(Utype) {
         if (Utype === 0) {
             return "customer";
@@ -17,60 +20,92 @@ function Login(props) {
             return "admin";
         }
     }
-    let type = handleUserType(typeUser)
-    return (
-        <div className={sign.Login}>
-            <div className={sign.Login}>
-                <div className={sign.LoginForm}>
-                    <div className={sign.logContent}>
-                        <h1>Đăng Nhập</h1>
-                        <div className={sign.field}>
-                            <Row>
-                                <label>Email</label>
-                            </Row>
-                            <Input name="Phone" type="email" required />
-                        </div>
-                        <div className={sign.field}>
-                            <div>
-                                <div className={sign.col}>
-                                    <label>Mật khẩu</label>
-                                </div>
-                                <div className={sign.col}>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-                                </div>
+    function onSubmit(event) {
+        event.preventDefault();
+        axios.get("http://localhost:3000/Client/username").then((res) => {
+            localStorage.setItem("id", res.data._id);
+            console.log(localStorage.getItem("id"));
+        });
+    }
+
+    let type = handleUserType(typeUser);
+    if (localStorage.getItem("id")) {
+        return (
+            <Switch>
+                <Redirect to={"/" + type + "/overview"} />
+            </Switch>
+        );
+    } else
+        return (
+            <div className={sign.Login}>
+                <div className={sign.Login}>
+                    <div className={sign.LoginForm}>
+                        <div className={sign.logContent}>
+                            <h1>Đăng Nhập</h1>
+                            <div className={sign.field}>
+                                <Row>
+                                    <label>Email</label>
+                                </Row>
+                                <Input
+                                    name="Phone"
+                                    type="email"
+                                    innerRef={(input) => setEmail(input)}
+                                    required
+                                />
                             </div>
-                            <Input name="pwd" type="password" required />
-                        </div>
-                        <div className={sign.field}>
-                            <Link to={"/" + type + "/overview"}>
-                                <Button onClick={props.onQuit} style={{ cursor: 'pointer' }}>
+                            <div className={sign.field}>
+                                <div>
+                                    <div className={sign.col}>
+                                        <label>Mật khẩu</label>
+                                    </div>
+                                    <div className={sign.col}></div>
+                                </div>
+                                <Input
+                                    name="pwd"
+                                    type="password"
+                                    innerRef={(input) => setPassword(input)}
+                                    required
+                                />
+                            </div>
+                            <div className={sign.field}>
+                                {/* <Link to={"/" + type + "/overview"}> */}
+                                <Button
+                                    onClick={onSubmit}
+                                    style={{ cursor: "pointer" }}
+                                >
                                     Đăng nhập
                                 </Button>
-                            </Link>
-                        </div>
-                        <div className={`${sign.field} ${sign.change}`}>
-                            <Row className={sign.ask}>
-                                <Link to={"/forgetpass"} >
-                                    <Button
-                                        className={`${sign.linkbutton} ${sign.switchForget}`}
-                                        onClick={props.onForget}
-                                        style={{ cursor: 'pointer' }}
-
-                                    >
-                                        Quên mật khẩu
-                                    </Button>
-                                </Link>
-                                <Link to={"/signup"}>
-                                    <Button className={sign.linkbutton} onClick={props.onSignup} style={{ cursor: 'pointer' }}>
-                                        Chưa có tài khoản? Đăng ký
-                                    </Button>
-                                </Link>
-                            </Row>
+                                {/* </Link> */}
+                            </div>
+                            <div className={`${sign.field} ${sign.change}`}>
+                                <Row className={sign.ask}>
+                                    <Link to={"/forgetpass"}>
+                                        <Button
+                                            className={`${sign.linkbutton} ${sign.switchForget}`}
+                                            onClick={props.onForget}
+                                            style={{ cursor: "pointer" }}
+                                        >
+                                            Quên mật khẩu
+                                        </Button>
+                                    </Link>
+                                    <Link to={"/signup"}>
+                                        <Button
+                                            className={sign.linkbutton}
+                                            onClick={props.onSignup}
+                                            style={{ cursor: "pointer" }}
+                                        >
+                                            Chưa có tài khoản? Đăng ký
+                                        </Button>
+                                    </Link>
+                                </Row>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
 }
 export default Login;
