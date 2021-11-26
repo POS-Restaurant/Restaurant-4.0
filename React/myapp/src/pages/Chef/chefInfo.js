3a78737693f097f31ba5d51a9516e06cbf49e2ee
duonglam306 <lamduong11201@gmail.com>
2021-11-26 12:39:16 +0700

    up

--- React/myapp/src/pages/Chef/chefInfo.js
+++ React/myapp/src/pages/Chef/chefInfo.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { dataChef } from '../../components/Account/Info/dataInfo'
 import Info from '../../components/Account/Info/info'
 function ChefInfo() {
     return (
         <div className="chefInfo">
-            <Info data={dataChef} type = {1} />
+            <Info type = {1} />
         </div>
     )
 }
