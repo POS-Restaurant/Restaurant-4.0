4989790cf091e4b89d95c0719295d50fbccefc13
duonglam306 <lamduong11201@gmail.com>
2021-11-26 09:23:37 +0700

    u

--- React/myapp/src/components/Account/viewOrder/OrderDetail.js
+++ React/myapp/src/components/Account/viewOrder/OrderDetail.js
@@ -37,7 +37,7 @@ function OrderDetail(props) {
                 <span className={detailOrder.orderId}> Mã đơn hàng: {props.id} </span>
                 <span className={detailOrder.timeOrder}>
                     <p>Thời gian đặt hàng: {props.timePurchase}</p>
-                    <p>Thời gian nhận hàng: {props.timeReceipt}</p>
+                    <p>{props.status === "Done" && "Thời gian nhận hàng:" + props.timeReceipt}</p>
                 </span>
                 <span className={detailOrder.customerName}>
                     Tên khách hàng: {props.name}
